44986c70d9b08451631755db3b51890fdb3c1ce2
nikhil_jung <nikhilbudhathoki0@gmail.com>
2023-12-09 20:35:17 +0545

    doing the components

--- src/App.js
+++ src/App.js
@@ -4,4 +4,16 @@ function App() {
   return <div></div>;
 }
 
+function Friends() {
+  return <div></div>;
+}
+
+function TotalAmount() {
+  return (
+    <div>
+      <h1>This is total amount in the context</h1>
+    </div>
+  );
+}
+
 export default App;
